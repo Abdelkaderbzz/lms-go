60c4b18405f9d01f191a27b9b6b71db79e56321a
abdelkaderbzz <bouzomitaabdelkader2004@gmail.com>
2025-12-19 08:10:17 +0100

    Add database connection and initialization

---
+++ internal/database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"lms-go/internal/models"
+)
+
+func InitDB(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return db, nil
+}
+
+func RunMigrations(db *gorm.DB) error {
+	return db.AutoMigrate(
+		&models.User{},
+		&models.Course{},
+		&models.Module{},
+		&models.Lesson{},
+		&models.Assignment{},
+		&models.Submission{},
+		&models.Grade{},
+		&models.Comment{},
+		&models.Quiz{},
+		&models.Question{},
+		&models.Option{},
+		&models.QuizAttempt{},
+		&models.QuizAnswer{},
+		&models.Enrollment{},
+		&models.Resource{},
+		&models.Announcement{},
+		&models.Discussion{},
+		&models.ForumPost{},
+		&models.ForumReply{},
+		&models.Notification{},
+		&models.LessonProgress{},
+		&models.Certificate{},
+	)
+}
